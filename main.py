141873a0b3ad05851a5fb22064772b068c2abc7d
Alfons8128 <122603501+Alfons8128@users.noreply.github.com>
2023-01-13 11:38:26 +0100

    Edit

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('hello')
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+camera = PiCamera()
+camera.rotation = 180
+
+camera.start_preview()
+sleep(5)
+camera.annotate_text = "Čus!"
+camera.image_effect = 'negative'
+camera.capture('/home/pi/Desktop/negativ.jpg')
+camera.stop_preview()
+
+# test
