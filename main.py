fe3823456504a6194dfc9017a0918c1b5927feb3
Alfons8128 <122603501+Alfons8128@users.noreply.github.com>
2023-01-13 15:24:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,2 @@
-from picamera import PiCamera
-from time import sleep
-
-camera = PiCamera()
-camera.rotation = 180
-
-camera.start_preview()
-sleep(100)
-camera.annotate_text = "Chus!"
-camera.image_effect = 'negative'
-camera.capture('./negativ.jpg')
-camera.stop_preview()
-print("Je to tam!")
-Naruto = True
-if Naruto:
-    print("Datebajo!")      
-# test
-# checking for experimental branch2
+wget -O - https://raw.githubusercontent.com/raspberrypilearning/image-id-coral/master/en/resources/install_script.sh | bash
+    
