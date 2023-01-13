1d86acc1eaf2453353b6eaa03952921a9ba7e460
matyasmatta <matyasmatta@email.cz>
2023-01-13 13:47:06 +0100

    První funkční verze

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
-camera = PiCamera()
 camera.rotation = 180
 
 camera.start_preview()
 sleep(5)
-camera.annotate_text = "Čus!"
+camera.annotate_text = "Chus!"
 camera.image_effect = 'negative'
-camera.capture('/home/pi/Desktop/negativ.jpg')
+camera.capture('./negativ.jpg')
 camera.stop_preview()
 
 # test
