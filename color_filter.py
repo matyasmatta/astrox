5158f5434d3fdfcf54c28b0fc72a6dca592c2368
matyasmatta <77171061+MMatty8137@users.noreply.github.com>
2023-01-20 13:55:32 +0100

    SenseHat

--- color_filter.py
+++ color_filter.py
@@ -2,7 +2,7 @@ from split_image import split_image
 import numpy as np
 import cv2
 
-frame = cv2.imread("photo.jpg")
+frame = cv2.imread(r"C:\Users\kiv\Documents\GitHub\astrox\dataset\crop\zchop.meta.x000.y485.n009.jpg")
 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 sensitivity = 90
 lower_white = np.array([0,0,255-sensitivity])
