b6a93642a696895a12484825f58d4eb9fd4186e2
matyasmatta <matyasmatta@email.cz>
2023-01-27 09:32:37 +0100

    Update eda.py

--- eda.py
+++ eda.py
@@ -1,4 +1,23 @@
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+#north = sense.get_compass()
+#print("North: %s" % north)
+
+# alternatives
+#print(sense.compass)
+
+#AstroX20220113
+
 while True:
- heading = sense.get_compass()
- if heading < 45 or heading 315:
-  sense.show_letter('N') 
+    acceleration = sense.get_accelerometer_raw()
+    x = acceleration["x"]
+    y = acceleration["y"]
+    z = acceleration["z"]
+
+    x=round(x, 5)
+    y=round(y, 5)
+    z=round(z, 5)
+    print(x, y, z)
+    sleep(0.5)
