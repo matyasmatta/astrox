9e0f9171b9703caca490623265512cf396dfdd71
matyasmatta <77171061+MMatty8137@users.noreply.github.com>
2023-02-15 13:23:10 +0100

    Shadow works!

--- stiny2.py
+++ stiny2.py
@@ -1,17 +1,19 @@
 from PIL import Image
 import numpy as np
 from numpy import average 
+from skyfield import api
+from skyfield import almanac
 
 # open specific cloud
-im = Image.open(r'C:\Users\kiv\Documents\GitHub\astrox\AO_2022.png') # Can be many different formats.
+im = Image.open(r'dataset\sample_crop\zchop.meta.x000.y000.n011.jpg') # Can be many different formats.
 pix = im.load()
 # get the width and hight of the image for iterating over
 print(im.size)  
 
 # set coordinates from AI model
-x = 1
-y = 1
-angle = 0 # set the angle (formatted as reduced edoov coefficient) for search, i.e. clockwise
+x = 294
+y = 199
+angle = 310 # set the angle (formatted as reduced edoov coefficient) for search, i.e. clockwise
 
 # calculate meta angle
 angle_radians =np.radians(angle)
@@ -59,7 +61,12 @@ y_increase_final_abs = abs(y_increase_final)
 y_sum = 0
 x_sum = 0
 count = 0
+limit = 50
+list_of_values = []
 
+# clear the whole txt file
+with open('stiny.txt', 'w') as f:
+    f.write("\n")
 # put quarter information back for pixel reading
 if q == 1:
     x_increase_final = x_increase_final
@@ -77,6 +84,7 @@ if q == 1:
             print(data)
             value = round(average(data))
             print(value)
+            list_of_values.append(value)
             # add to y_sum and move pixel x for 1
             x += 1
             y_sum += y_increase_final_abs
@@ -85,6 +93,7 @@ if q == 1:
             print(data)
             value = round(average(data))
             print(value)
+            list_of_values.append(value)
             x += 1
             y -= 1
         if x_increase_final_abs < y_increase_final_abs:
@@ -97,6 +106,7 @@ if q == 1:
             print(data)
             value = round(average(data))
             print(value)
+            list_of_values.append(value)
             # add to y_sum and move pixel x for 1
             y -= 1
             x_sum += x_increase_final_abs
@@ -105,7 +115,7 @@ if q == 1:
             value = str(value)
             f.write(value)
             f.write("\n")
-        if count > 20:
+        if count > limit:
             break
 if q == 2:
     x_increase_final = x_increase_final
@@ -123,6 +133,7 @@ if q == 2:
             print(data)
             value = round(average(data))
             print(value)
+            list_of_values.append(value)
             # add to y_sum and move pixel x for 1
             x += 1
             y_sum += y_increase_final_abs
@@ -131,6 +142,7 @@ if q == 2:
             print(data)
             value = round(average(data))
             print(value)
+            list_of_values.append(value)
             x += 1
             y += 1
         if x_increase_final_abs < y_increase_final_abs:
@@ -143,6 +155,7 @@ if q == 2:
             print(data)
             value = round(average(data))
             print(value)
+            list_of_values.append(value)
             # add to y_sum and move pixel x for 1
             y += 1
             x_sum += x_increase_final_abs
@@ -151,7 +164,7 @@ if q == 2:
             value = str(value)
             f.write(value)
             f.write("\n")
-        if count > 20:
+        if count > limit:
             break
 if q == 3:
     x_increase_final = -x_increase_final
@@ -169,6 +182,7 @@ if q == 3:
             print(data)
             value = round(average(data))
             print(value)
+            list_of_values.append(value)
             # add to y_sum and move pixel x for 1
             x -= 1
             y_sum += y_increase_final_abs
@@ -177,6 +191,7 @@ if q == 3:
             print(data)
             value = round(average(data))
             print(value)
+            list_of_values.append(value)
             x -= 1
             y += 1
         if x_increase_final_abs < y_increase_final_abs:
@@ -189,6 +204,7 @@ if q == 3:
             print(data)
             value = round(average(data))
             print(value)
+            list_of_values.append(value)
             # add to y_sum and move pixel x for 1
             y += 1
             x_sum += x_increase_final_abs
@@ -197,7 +213,7 @@ if q == 3:
         with open('stiny.txt', 'a') as f:
             f.write(value)
             f.write("\n")
-        if count > 20:
+        if count > limit:
             break
 if q == 4:
     x_increase_final = -x_increase_final
@@ -215,6 +231,7 @@ if q == 4:
             print(data)
             value = round(average(data))
             print(value)
+            list_of_values.append(value)
             # add to y_sum and move pixel x for 1
             x -= 1
             y_sum += y_increase_final_abs
@@ -223,6 +240,7 @@ if q == 4:
             print(data)
             value = round(average(data))
             print(value)
+            list_of_values.append(value)
             x -= 1
             y -= 1
         if x_increase_final_abs < y_increase_final_abs:
@@ -235,14 +253,17 @@ if q == 4:
             print(data)
             value = round(average(data))
             print(value)
+            list_of_values.append(value)
+            list_of_values.append(value)
             # add to y_sum and move pixel x for 1
             y -= 1
             x_sum += x_increase_final_abs
         #write into txt
         with open('stiny.txt', 'a') as f:
+            value = str(value)
             f.write(value)
             f.write("\n")
-        if count > 20:
+        if count > limit:
             break
 
 # print for debugging
@@ -251,3 +272,40 @@ print(x_increase_final, y_increase_final)
 # set absolute final values
 x_increase_final_abs = abs(x_increase_final)
 y_increase_final_abs = abs(x_increase_final)
+
+print(list_of_values)
+shadow_low = min(list_of_values)
+cloud_high = max(list_of_values)
+
+shadow_location = list_of_values.index(shadow_low)
+cloud_location = list_of_values.index(cloud_high)
+
+shadow_lenght = shadow_location - cloud_location
+print(shadow_lenght)
+
+def distance(fieldOfView, distanceinpixels):
+    fieldOfViewRadians = fieldOfView*(np.pi/180)
+    distanceinmeters = int(distanceinpixels)*142
+    return distanceinmeters
+
+lenght = distance(60, shadow_lenght)
+print("Shadow has exactly", lenght, "meters!")
+      
+ts = api.load.timescale()
+ephem = api.load("ftp://ssd.jpl.nasa.gov/pub/eph/planets/bsp/de421.bsp")
+
+sun = ephem["Sun"]
+earth = ephem["Earth"]
+
+location = api.Topos("49.73880 N", "13.39350 E", elevation_m=500)
+sun_pos = (earth + location).at(ts.now()).observe(sun).apparent()
+altitude, azimuth, distance = sun_pos.altaz()
+
+print(f"Azimuth: {azimuth.degrees:.4f}")
+print(f"Altitude: {altitude.degrees:.4f}")
+
+altitude= int(altitude.degrees)
+cloudheight = np.tan(altitude)*lenght
+
+print("Shadow is exactly", cloudheight, "meters from ground!")
+
