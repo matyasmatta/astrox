a6da6704b06eb478d1196369b3f8d7d53bb9acf9
matyasmatta <77171061+MMatty8137@users.noreply.github.com>
2023-02-16 15:14:49 +0100

    Second pre-final software, everything seems to be alright in theory but in practice it's hell. EXIF does not work yet!

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
+import os
+import pathlib
+from pycoral.utils import edgetpu
+from pycoral.utils import dataset
+from pycoral.adapters import common
+from pycoral.adapters import classify
+from PIL import Image
+import ai
 import stiny_maty
-import sever_eda
 
-data = stiny_maty.sun_data.altitude("34.28614 S", "147.9849 E", 2022, 1, 15, 5, 16, 50)
-print(data)
+try:
+    image = 'zchop.meta.x000.y000.n004.jpg'
+    data = ai.ai_model(image)
+    print(data)
+    counter_for_shadows = 0
+    angle = 317
+    while True:
+        try:
+            x_max = data[counter_for_shadows]['xmax']
+            y_max = data[counter_for_shadows]['ymax']
+            x_min = data[counter_for_shadows]['xmin']
+            y_min = data[counter_for_shadows]['ymin']
+            print(x_min, y_min, x_max, y_max)
 
-data = stiny_maty.calculate_shadow('.\\zchop.meta.x000.y000.n011.jpg', 294,199,310)
-print(data)
+            x_centre_of_cloud = (x_min+x_max)/2
+            y_centre_of_cloud = (y_min+y_max)/2
+            x_centre_of_cloud = round(x_centre_of_cloud, 0)
+            y_centre_of_cloud = round(y_centre_of_cloud, 0)
+            x_centre_of_cloud = int(x_centre_of_cloud)
+            y_centre_of_cloud = int(y_centre_of_cloud)
 
-data, coefficient = sever_eda.find_north(".\\namibie1.jpg", ".\\namibie2.jpg", "-25.07194", "-25.49306")
-print(data)
+            print(x_centre_of_cloud, y_centre_of_cloud)
+            data[counter_for_shadows]['shadow'] = stiny_maty.calculate_shadow(image, x_centre_of_cloud, y_centre_of_cloud, angle)
+            print("Cloud number", counter_for_shadows, "has a lenght of", data[counter_for_shadows]['shadow'])
+            counter_for_shadows += 1
+        except:
+            break
+except:
+    print("code failed")
