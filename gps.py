c2773b9c8beb805cfdd4d07744b8919ca4017b94
matyasmatta <77171061+MMatty8137@users.noreply.github.com>
2023-02-20 10:55:22 +0100

    Update gps.py

--- gps.py
+++ gps.py
@@ -1,10 +1,16 @@
 #Program na zjištění souřadnic mraku
 import numpy as np
+from PIL import Image
+
+image = Image.open(r"C:\Users\trajc\Downloads\51844772672_92599fb67a_o.jpg")
+rotated_image = rotated_image.rotate(225)
+rotated_image.show()
+rotated_image = rotated_image.save("otocena_fotka.jpg")
 def cloudPosition(relativeLatitude, relativeLongtitude, centerCoordinates, cloudCoordinates): #centerCoordinates and cloudCoordinates in xxx.xx, xxx.xx format
     #input hodnot
     meridionalCircumference = 40008
     earthCircumference = 40075
-    k = 0.142                                                                           #konstanta pro převod pixelů na km
+    k = 0.12648                                                                           #konstanta pro převod pixelů na km
 #    relativeLatitude = input("z. š. středu fotky ve formátu DD MM SS.SS N/S: ")       #input z. š. středu fotky
 #    relativeLongtitude = input("z. d. středu fotky ve formátu DDD MM SS.SS E/W: ")      #input z. d. středu fotky
 
@@ -30,4 +36,4 @@ def cloudPosition(relativeLatitude, relativeLongtitude, centerCoordinates, cloud
     cloudLongitude = float(decimalLongitude) + (distanceX*360)/(earthCircumference*np.cos(cloudLatitude * (np.pi/180)))
     print("z. d.:", cloudLongitude)
 
-cloudPosition("55 55 55 N", "56 56 56 E", "55.5, 565", "35, 25")
+cloudPosition("33 28 14.6 S", "125 24 03.9 E", "2366, 2251", "903, 1780")
