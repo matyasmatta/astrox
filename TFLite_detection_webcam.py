54b18fc7d4b7b18397f7a401a04dffc8bd6f74c6
matyasmatta <matyasmatta@email.cz>
2023-04-06 13:47:21 +0200

    Update

--- TFLite_detection_webcam.py
+++ TFLite_detection_webcam.py
@@ -247,7 +247,7 @@ while True:
         stin_centre_y = (stin_ymin+stin_ymax)/2
         vzdalenost_x = abs(mrak_centre_x - stin_centre_x)
         vzdalenost_y = abs(mrak_centre_y - stin_centre_y)
-        vzdalenost_prepona_px = np.sqrt(vzdalenost.x^2 + vzdalenost.y^2)
+        vzdalenost_prepona_px = np.sqrt(vzdalenost_x^2 + vzdalenost_y^2)
         vzdalenost_prepona_cm = vzdalenost_prepona_px/85
 
         print("prepona", vzdalenost_prepona_cm)
