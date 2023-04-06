c42f119c43997ab614a652292b0df13f392d3213
matyasmatta <matyasmatta@email.cz>
2023-04-06 13:35:18 +0200

    Updated text

--- TFLite_detection_webcam.py
+++ TFLite_detection_webcam.py
@@ -254,9 +254,9 @@ while True:
         # Draw framerate in corner of frame
         text = "Výška: "+vyska
     except:
-        pass
+        text = "Error"
 
-    cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
+    cv2.putText(frame,text,(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
 
     # All the results have been drawn on the frame, so it's time to display it.
     cv2.imshow('Object detector', frame)
