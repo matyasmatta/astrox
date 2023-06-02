316ee47a6335fbad194dd7dc8e1c4b676f9d1ccc
matyasmatta <matyasmatta@email.cz>
2023-06-02 20:44:39 +0200

    Python upload with broken git

--- earth_processing.py
+++ earth_processing.py
@@ -6,6 +6,13 @@ from skyfield import almanac
 import os
 import json
 import csv
+import PIL 
+from PIL import ImageEnhance
+from PIL import Image
+# import the modules
+import os
+from os import listdir
+from PIL import Image
 
 # ignore the 'json' in the name, we replaced the file format with csv for simplicity, jsons are useful for export data - not metadata
 def write_pixel_into_json(count, x, y, cloud_id="not specified", image_id="not specified"):
@@ -360,6 +367,51 @@ def calculate_shadow(file_path, x, y, angle, cloud_id="not specified", image_id=
     # print("Shadow is exactly", cloudheight, "meters from ground!")
     # print(list_of_values)
     return cloudheight
+
+class app:
+    photoID = 0
+    def
+    # get the path or directory
+    folder_dir = r"C:\Users\kiv\Documents\GitHub\astrox\dataset\sample_original"
+    for images in os.listdir(folder_dir):
+        photoID += 1
+        # check if the image ends with png or jpg or jpeg
+        if (images.endswith(".png") or images.endswith(".jpg")\
+            or images.endswith(".jpeg")):
+            # display
+            print(images)
+            # Opens a image in RGB mode
+            im = Image.open("./dataset/original/" + images)
+            left = 1075
+            top = 520
+            right = 3015
+            bottom = 2460
+            # Cropped image of above dimension
+            # (It will not change original image)
+            im1 = im.crop((left, top, right, bottom))
+            # Shows the image in image viewer
+            im1.save('meta.bmp')
+            # Slice
+            infile = 'meta.jpg'
+            chopsize = 485
+
+            img = Image.open(infile)
+            width, height = img.size
+
+            # Metadata
+            image = Image.open('./dataset/original/51844762822_3b10505c80_o.jpg')
+            exif = image.info['exif']
+
+            # Save Chops of original image
+            for x0 in range(0, width, chopsize):
+                for y0 in range(0, height, chopsize):
+                    box = (x0, y0,
+                            x0+chopsize if x0+chopsize <  width else  width - 1,
+                            y0+chopsize if y0+chopsize < height else height - 1)
+                    print('%s %s' % (infile, box))
+                    img.crop(box).save('./dataset/crop/zchop.%s.x%03d.y%03d.n%03d.jpg' % (infile.replace('.jpg',''), x0, y0, int(photoID)),exif=exif)
+
+
 
 if __name__ == '__main__':
     cloudheight = calculate_shadow('zchop.meta.x000.y000.n011.jpg', 294,199,360)
---
+++ split.py
@@ -0,0 +1,50 @@
+import PIL 
+from PIL import ImageEnhance
+from PIL import Image
+# import the modules
+import os
+from os import listdir
+from PIL import Image
+
+photoID = 0
+# get the path or directory
+folder_dir = r"C:\Users\kiv\Documents\GitHub\astrox\data"
+for images in os.listdir(folder_dir):
+    photoID += 1
+    # check if the image ends with png or jpg or jpeg
+    if (images.endswith(".png") or images.endswith(".jpg")\
+        or images.endswith(".jpeg")):
+        # display
+        print(images)
+        # Opens a image in RGB mode
+        im = Image.open("./data/" + images)
+        left = 16
+        top = 10
+        right = 4056
+        bottom = 3040
+        # Cropped image of above dimension
+        # (It will not change original image)
+        im1 = im.crop((left, top, right, bottom))
+        # Shows the image in image viewer
+        im1.save('meta.jpg')
+        # Slice
+        infile = 'meta.jpg'
+        chopsize = 505
+
+        img = Image.open(infile)
+        width, height = img.size
+
+        # Metadata
+        exif = im.info['exif']
+        images = images.split(sep='.jpg')
+
+        # Save Chops of original image
+        for x0 in range(0, width, chopsize):
+            for y0 in range(0, height, chopsize):
+                box = (x0, y0,
+                        x0+chopsize if x0+chopsize <  width else  width - 1,
+                        y0+chopsize if y0+chopsize < height else height - 1)
+                print('%s %s' % (infile, box))
+
+                images_path = './data_chop/' + images[0] + "_x" + str(x0) + "_y" + str(y0) + ".jpg"
+                img.crop(box).save(images_path,exif=exif)
