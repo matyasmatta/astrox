080c7e95e91b2a53b1e2590d8bc2aeccfd1fadbb
Plice13 <edacoc@seznam.cz>
2023-06-14 00:11:26 +0200

    jdu spát a musím nad tím přemýšlet :(

--- eda/main1.py
+++ eda/main1.py
@@ -92,8 +92,76 @@ def fill():
     cv2.fillPoly(img, [points], (255, 255, 255))
     cv2.imshow('image', img)    
 
-def manual():
+
+def get_sun(image_path):
+    def get_latitude(image):
+        with open(image, 'rb') as image_file:
+            img = exify(image_file)
+            try:
+                latitude = img.get("gps_latitude")
+                latitude_ref = img.get("gps_latitude_ref")
+                if latitude == None:
+                    latitude, latitude_ref = (0.0, 0.0, 0.0), "A"
+            except AttributeError:
+                latitude, latitude_ref = (0.0, 0.0, 0.0), "A"
+        decimal_degrees = latitude[0] + latitude[1] / 60 + latitude[2] / 3600
+        latitude_formatted = str(str(decimal_degrees)+" "+str(latitude_ref))
+        return latitude_formatted
+
+    def get_longitude(image):
+        with open(image, 'rb') as image_file:
+            img = exify(image_file)
+            try:
+                longitude = img.get("gps_longitude")
+                longitude_ref = img.get("gps_longitude_ref")
+                if longitude == None:
+                    longitude, longitude_ref = (0.0, 0.0, 0.0), "A"
+            except AttributeError:
+                longitude, longitude_ref = (0.0, 0.0, 0.0), "A"
+        decimal_degrees = longitude[0] + longitude[1] / 60 + longitude[2] / 3600
+        longitude_formatted = str(str(decimal_degrees)+" "+longitude_ref)
+        return longitude_formatted   
+    # this is a small class containing a method for date extraction that is not via the EXIF library but the PIL library
+    def find_time_from_image(image_path):
+        img = Image.open(image_path)
+        img_exif_dict = img.getexif()
+        date = str(img_exif_dict[306])
+
+        year = int(date[0:4])
+        month = int(date[5:7])
+        day = int(date[8:10])
+        hour = int(date[11:13])
+        minute = int(date[14:16])
+        second = int(date[17:19])
+
+        return year, month, day, hour, minute, second
+
+    def altitude(coordinates_latitude, coordinates_longtitude, year, month, day, hour, minute, second):
+        # use the NASA API to be able to calculate sun's position
+        ts = api.load.timescale()
+        ephem = api.load("de421.bsp")
+
+        # define sky objects
+        sun = ephem["Sun"]
+        earth = ephem["Earth"]
+
+        # given coordinates calculate the altitude (how many degrees sun is above the horizon), additional data is redundant
+        location = api.Topos(coordinates_latitude, coordinates_longtitude, elevation_m=500)
+        sun_pos = (earth + location).at(ts.tt(year,month,day,hour,minute,second)).observe(sun).apparent()
+        altitude, azimuth, distance = sun_pos.altaz()
+        altitude= float(altitude.degrees)
+        return(altitude)
+    year, month, day, hour, minute, second = find_time_from_image(image_path)
+    latitude = get_latitude(image_path)
+    longitude = get_longitude(image_path)
+
+    # now we calculate the sun altitude using a function
+    altitude2 = altitude(latitude, longitude, year, month, day, hour, minute, second)
+    return altitude2
+
+def main():
     global img, ultra_destroy, cisilko, list_of_cisilko
+    ultra_destroy = 0
     global first_point, second_point
     first_point = (0,0)
     second_point = (0,0)
@@ -251,78 +319,6 @@ def manual():
 
     # close the window
     cv2.destroyAllWindows()
-
-def get_sun(image_path):
-    def get_latitude(image):
-        with open(image, 'rb') as image_file:
-            img = exify(image_file)
-            try:
-                latitude = img.get("gps_latitude")
-                latitude_ref = img.get("gps_latitude_ref")
-                if latitude == None:
-                    latitude, latitude_ref = (0.0, 0.0, 0.0), "A"
-            except AttributeError:
-                latitude, latitude_ref = (0.0, 0.0, 0.0), "A"
-        decimal_degrees = latitude[0] + latitude[1] / 60 + latitude[2] / 3600
-        latitude_formatted = str(str(decimal_degrees)+" "+str(latitude_ref))
-        return latitude_formatted
-
-    def get_longitude(image):
-        with open(image, 'rb') as image_file:
-            img = exify(image_file)
-            try:
-                longitude = img.get("gps_longitude")
-                longitude_ref = img.get("gps_longitude_ref")
-                if longitude == None:
-                    longitude, longitude_ref = (0.0, 0.0, 0.0), "A"
-            except AttributeError:
-                longitude, longitude_ref = (0.0, 0.0, 0.0), "A"
-        decimal_degrees = longitude[0] + longitude[1] / 60 + longitude[2] / 3600
-        longitude_formatted = str(str(decimal_degrees)+" "+longitude_ref)
-        return longitude_formatted   
-    # this is a small class containing a method for date extraction that is not via the EXIF library but the PIL library
-    def find_time_from_image(image_path):
-        img = Image.open(image_path)
-        img_exif_dict = img.getexif()
-        date = str(img_exif_dict[306])
-
-        year = int(date[0:4])
-        month = int(date[5:7])
-        day = int(date[8:10])
-        hour = int(date[11:13])
-        minute = int(date[14:16])
-        second = int(date[17:19])
-
-        return year, month, day, hour, minute, second
-
-    def altitude(coordinates_latitude, coordinates_longtitude, year, month, day, hour, minute, second):
-        # use the NASA API to be able to calculate sun's position
-        ts = api.load.timescale()
-        ephem = api.load("de421.bsp")
-
-        # define sky objects
-        sun = ephem["Sun"]
-        earth = ephem["Earth"]
-
-        # given coordinates calculate the altitude (how many degrees sun is above the horizon), additional data is redundant
-        location = api.Topos(coordinates_latitude, coordinates_longtitude, elevation_m=500)
-        sun_pos = (earth + location).at(ts.tt(year,month,day,hour,minute,second)).observe(sun).apparent()
-        altitude, azimuth, distance = sun_pos.altaz()
-        altitude= float(altitude.degrees)
-        return(altitude)
-    year, month, day, hour, minute, second = find_time_from_image(image_path)
-    latitude = get_latitude(image_path)
-    longitude = get_longitude(image_path)
-
-    # now we calculate the sun altitude using a function
-    altitude = altitude(latitude, longitude, year, month, day, hour, minute, second)
-    return altitude
-
-def main():
-    global ultra_destroy
-    ultra_destroy = 0
-    manual()
-
 
 if __name__=="__main__":
     # 0 for Maty, 1 for Eda
